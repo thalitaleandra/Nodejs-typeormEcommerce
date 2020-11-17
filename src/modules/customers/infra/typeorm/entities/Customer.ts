b596e375d709b401544d0572a74d4fe803b9c255
thalitaleandra <thalita@geowellex.com>
2020-11-17 18:07:38 -0300

    version 1.0

--- src/modules/customers/infra/typeorm/entities/Customer.ts
+++ src/modules/customers/infra/typeorm/entities/Customer.ts
@@ -6,15 +6,17 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 
+@Entity('customers')
 class Customer {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
-
+  @Column()
   name: string;
-
+  @Column()
   email: string;
-
+  @CreateDateColumn()
   created_at: Date;
-
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
